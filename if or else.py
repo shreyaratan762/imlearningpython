d9cc99ae2ccc67607ffa955346242ab8444d7aee
S Shreya Ratan <shreyaratan762@gmail.com>
2025-08-26 14:12:56 +0530

    Add files via upload

---
+++ if or else.py
@@ -0,0 +1,13 @@
+per = int(input("Enter your percentage: "))
+if per >= 70:
+    print("first division")
+elif per >= 60:
+    print("second division" )
+elif per >= 50:
+    print("third division" )
+elif per >= 40:
+    print("fourth division")
+
+    
+else:
+    print("fail")
