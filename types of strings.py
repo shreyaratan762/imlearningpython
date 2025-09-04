9689b35a9d14519bf38a250cb0b1857762d289f6
S Shreya Ratan <shreyaratan762@gmail.com>
2025-09-04 23:08:12 +0530

    Add files via upload
    strings.py

---
+++ types of strings.py
@@ -0,0 +1,17 @@
+a = "Hello, World!"
+print(a.replace("H" , "h"))
+
+b = "Hello world"
+print(b.upper())
+print(b.lower())
+
+c = "Hello world"
+print(c[2:5])
+
+d = "Hello world"
+print(d[2:])
+
+#to change a value in a string
+cars = ["Ford", "Toyota" , "Honda"]
+cars [2] = "Skoda"
+print(cars)
